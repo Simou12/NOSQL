516b3b6509feb0c30f2316a9f35f776094dc92ce
bou <ib_sari@esi.dz>
2023-12-08 15:53:52 +0100

    structure mliha and mazal les stats

--- projet/qengine-master/src/main/java/qengine/program/Main.java
+++ projet/qengine-master/src/main/java/qengine/program/Main.java
@@ -1,17 +1,22 @@
 package qengine.program;
 
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.apache.jena.query.Query;
@@ -75,12 +80,28 @@ final class Main {
 	/**
 	 * Fichier contenant les requêtes sparql
 	 */
-	static final String queryFile = workingDir + "STAR_ALL_workload.queryset";
-
+	static String queryFile = workingDir + "sample_query.queryset";
+											//STAR_ALL_workload
 	/**
 	 * Fichier contenant des données rdf
 	 */
-	static final String dataFile = workingDir + "100K.nt";
+	static String dataFile = workingDir + "sample_data.nt";
+
+	static HashSet<String> queryFiles;
+	static String queryFolder;
+	static boolean useFolder = false; 
+
+	static String outputDir = "output/";
+
+	static boolean useJena = false;
+	static boolean shuffle = false;
+	static int warm = 0;
+
+	static String csvFile = outputDir+"csvfile.csv";
+
+	static String statscsv = outputDir+"statscsv.csv";
+
+	
 	
 	
 	
@@ -182,65 +203,139 @@ final class Main {
 	 * Entrée du programme
 	 */
 	public static void main(String[] args) throws Exception {
-		System.out.println("hello");
-		System.out.println("loading data into this system");
-		MainRDFHandler mainRdfHandler = parseData();
-		System.out.println("loading queries and checking them using this system");
-		Object[] resultzzz = parseQueries(mainRdfHandler);
-		List<HashSet<String>> results = (List<HashSet<String>>)resultzzz[0];
-		List<String> requettes = (List<String>)resultzzz[1];
 
-		Model model = ModelFactory.createDefaultModel();
-		System.out.println("loading data into Jena");
-		model.read(dataFile,"N-TRIPLET");
-		System.out.println("checking queries using Jena");
-		List<HashSet<String>> results_jena = parseQueriesJena(model);
-
-		int len = results.size();
-		Boolean complet = true;
-		List<Integer> kaka = new ArrayList<>();
-
-		System.out.println("checked " +  len + " queries");
-
-		for (int i=0 ; i<len ; i++){
-			//System.out.println(i);
-			if (!(results.get(i) == null) && !(results_jena.get(i) == null)){
-							
-				if (!results.get(i).equals(results_jena.get(i))){
-					complet = false ;
-					kaka.add(i);
-					//System.out.println("M E R D E");
-				}else{
-					//System.out.println("H A M D O U L E H");
-				}		
+		for (int i = 0; i < args.length; i++) {
+			switch (args[i]) {
+			case "-queries":
+				useFolder = true;
+				queryFolder =args[++i];
+				queryFiles = new HashSet(Stream.of(new File(queryFolder ).listFiles()).filter(file -> !file.isDirectory()).map(File::getName).collect(Collectors.toSet()));
+				System.out.println(queryFolder);
+				// Gérer le chemin des requêtes
+				break;
+			case "-data":
+				dataFile = args[++i];
+				// Gérer le chemin des données
+				break;
+			case "-output":
+				outputDir = args[++i];
+				csvFile = outputDir +"csvfile.csv";
+				statscsv = outputDir+"statscsv.csv";
+				break;
+			case "-jena":
+				useJena = true;
+				break;
+			case "-warm":
+				warm = Integer.parseInt(args[++i]);
+				// activer la fonction warm (chauffer le système)
+				break;
+			case "-shuffle":
+				shuffle = true;
+				// activer la fonction shuffle
+				break;
+			default:
+				
+				System.err.println("Mauvais argument");
+				//return;
 			}
 		}
 		
-		if (complet) {
-			System.out.println("H A M D O U L E H");
+		System.out.println("loading queries");
+		List<String> requettes = new ArrayList<String>();
+		if (useFolder) {
+			for (String file : queryFiles){
+				requettes.addAll(parseQueries(queryFolder+"/"+file));
+			}
+
 		}
 		else{
-			System.out.println("M E R D E");
+			requettes = parseQueries(queryFile);
+		}
 
 
-			for (int i:kaka){
-				System.out.println("            case : "+ i);
-				System.out.println();
-				System.out.println(requettes.get(i));
-				System.out.println();
-				System.out.println(" 			our : ");
-				for (String value:results.get(i)){
-					System.out.println(value);
+		if(shuffle){
+			System.out.println("shuffling");
+			Collections.shuffle(requettes);
+		}
+
+		System.out.println("loading data into this system");
+		MainRDFHandler mainRdfHandler = parseData();
+
+
+		if(warm>0){
+			System.out.println("	warming up this system");
+			warmUpSystem(mainRdfHandler, requettes, warm);
+		}
+
+
+		System.out.println("	checking queries using this system");
+		List<HashSet<String>> results = processQueries(mainRdfHandler, requettes);
+
+
+		if (useJena){
+			Model model = ModelFactory.createDefaultModel();
+			System.out.println("Jena :");
+			System.out.println("	loading data into Jena");
+			model.read(dataFile,"N-TRIPLET");
+
+			if(warm>0){
+				System.out.println("	warming up Jena");
+				warmUpJena(model, requettes, warm);
+			}
+
+			System.out.println("	checking queries using Jena");
+			List<HashSet<String>> results_jena = parseQueriesJena(model,requettes);
+
+			int len = results.size();
+			Boolean complet = true;
+			List<Integer> kaka = new ArrayList<>();
+
+			System.out.println("	checked " +  len + " queries");
+
+			for (int i=0 ; i<len ; i++){
+				//System.out.println(i);
+				if (!(results.get(i) == null) && !(results_jena.get(i) == null)){
+								
+					if (!results.get(i).equals(results_jena.get(i))){
+						complet = false ;
+						kaka.add(i);
+						//System.out.println("M E R D E");
+					}else{
+						//System.out.println("H A M D O U L E H");
+					}		
 				}
-				System.out.println(" 			JENA : ");
-				for (String value:results_jena.get(i)){
-					System.out.println(value);
+			}
+			
+			if (complet) {
+				System.out.println("	the system is complete");
+			}
+			else{
+				System.out.println("	the system is caught lacking :");
+
+
+				for (int i:kaka){
+					System.out.println("            case : "+ i);
+					System.out.println();
+					System.out.println(requettes.get(i));
+					System.out.println();
+					System.out.println(" 			our : ");
+					for (String value:results.get(i)){
+						System.out.println(value);
+					}
+					System.out.println(" 			JENA : ");
+					for (String value:results_jena.get(i)){
+						System.out.println(value);
+					}
 				}
 			}
 
-			if (!mainRdfHandler.index.ops.containsKey(1935)){System.out.println("WHAT WHY WHERE");}
+
 		}
 
+
+		System.out.println("printing to " + csvFile);
+		resultsToCsv(csvFile, requettes, results);
+		
 
 
 	}
@@ -252,7 +347,7 @@ final class Main {
 	/**
 	 * Traite chaque requête lue dans {@link #queryFile} avec {@link #processAQuery(ParsedQuery)}.
 	 */
-	private static Object[] parseQueries(MainRDFHandler mainRdfHandler) throws FileNotFoundException, IOException {
+	private static List<String> parseQueries(String filepath) throws FileNotFoundException, IOException {
 		/**
 		 * Try-with-resources
 		 * 
@@ -262,12 +357,10 @@ final class Main {
 		 * On utilise un stream pour lire les lignes une par une, sans avoir à toutes les stocker
 		 * entièrement dans une collection.
 		 */
-		try (Stream<String> lineStream = Files.lines(Paths.get(queryFile))) {
-			SPARQLParser sparqlParser = new SPARQLParser();
+		try (Stream<String> lineStream = Files.lines(Paths.get(filepath))) {
 			Iterator<String> lineIterator = lineStream.iterator();
 			StringBuilder queryString = new StringBuilder();
 			List<String> requettes = new ArrayList<>();
-			List<HashSet<String>> all_results = new ArrayList<HashSet<String>>();
 			
 			while (lineIterator.hasNext())
 			
@@ -280,53 +373,42 @@ final class Main {
 				queryString.append(line);
 
 				if (line.trim().endsWith("}")) {
-					ParsedQuery query = sparqlParser.parseQuery(queryString.toString(), baseURI);
-					
 					requettes.add(queryString.toString());
-					HashSet<String> results = processAQuery(query,mainRdfHandler); // Traitement de la requête, à adapter/réécrire pour votre programme
-					all_results.add(results);
-
 					queryString.setLength(0); // Reset le buffer de la requête en chaine vide
 				}
 			}
-			Object[] resultzzz = new Object[2];
-			resultzzz[0] = all_results;
-			resultzzz[1]=requettes;
-			return resultzzz;
+
+			return requettes;
 		}
 	}
 
-	
-	private static List<HashSet<String>> parseQueriesJena(Model model) throws FileNotFoundException, IOException {
-		
-		try (Stream<String> lineStream = Files.lines(Paths.get(queryFile))) {
-			Iterator<String> lineIterator = lineStream.iterator();
-			StringBuilder queryString = new StringBuilder();
+	private static List<HashSet<String>> processQueries(MainRDFHandler mainRdfHandler,List<String> requettes) throws FileNotFoundException, IOException {
+
+			SPARQLParser sparqlParser = new SPARQLParser();
 			List<HashSet<String>> all_results = new ArrayList<HashSet<String>>();
 			
-			while (lineIterator.hasNext())
 			
-			/*
-			 * On stocke plusieurs lignes jusqu'à ce que l'une d'entre elles se termine par un '}'
-			 * On considère alors que c'est la fin d'une requête
-			 */
-			{
-				String line = lineIterator.next();
-				queryString.append(line);
-				String query_str = queryString.toString();
+			for(String requette:requettes){
+				ParsedQuery query = sparqlParser.parseQuery(requette, baseURI);
+				HashSet<String> results = processAQuery(query,mainRdfHandler); // Traitement de la requête, à adapter/réécrire pour votre programme
+				all_results.add(results);
+			}
 
-				if (line.trim().endsWith("}")) {
+			return all_results;
+	
+	}
 
-					all_results.add(processJENA(query_str, model));
-					queryString.setLength(0); // Reset le buffer de la requête en chaine vide
-				}
+	
+	private static List<HashSet<String>> parseQueriesJena(Model model,List<String> requettes) throws FileNotFoundException, IOException {
+			List<HashSet<String>> all_results = new ArrayList<HashSet<String>>();
+
+			for(String requette:requettes){
+				all_results.add(processJENA(requette, model));
 			}
 			return all_results;
-		}
+		
 	}
-
 	
-
 	/**
 	 * Traite chaque triple lu dans {@link #dataFile} avec {@link MainRDFHandler}.
 	 */
@@ -368,5 +450,78 @@ final class Main {
 		} 	
 	}
 	
+	private static void warmUpSystem(MainRDFHandler mainRdfHandler,List<String> queries,int warmPercentage) throws IOException {
+        if (warmPercentage <= 0 || warmPercentage > 100) {
+            System.out.println("		Invalide pourcentage warm-up, warm-up ignoré.");
+            return;
+        }
+        int numberOfQueriesToRun = (int) Math.ceil((double) queries.size() * warmPercentage / 100);
+        System.out.println("		warming with " + numberOfQueriesToRun+ " queries");
+        // Collections.shuffle(queries); // Mélanger la liste des requêtes
+        List<String> selectedQueries = queries.stream().limit(numberOfQueriesToRun).collect(Collectors.toList());
+		SPARQLParser sparqlParser = new SPARQLParser();
+
+        for (String query : selectedQueries) {
+            // System.out.println("je warm avec la requete: " + query);
+			// Traitement de la requête pour le warm-up
+			
+			ParsedQuery p_query = sparqlParser.parseQuery(query, baseURI);
+			processAQuery(p_query,mainRdfHandler);
+        }
+        System.out.println("		Warm up terminé");
+    }
+	
+	private static void warmUpJena(Model model,List<String> queries,int warmPercentage) throws IOException {
+        if (warmPercentage <= 0 || warmPercentage > 100) {
+            System.out.println("		Invalide pourcentage warm-up, warm-up ignoré.");
+            return;
+        }
+        int numberOfQueriesToRun = (int) Math.ceil((double) queries.size() * warmPercentage / 100);
+        System.out.println("		warming with " + numberOfQueriesToRun+ " queries");
+        // Collections.shuffle(queries); // Mélanger la liste des requêtes
+        List<String> selectedQueries = queries.stream().limit(numberOfQueriesToRun).collect(Collectors.toList());
+		SPARQLParser sparqlParser = new SPARQLParser();
+		
+        for (String query : selectedQueries) {
+			processJENA(query, model);
+        }
+        System.out.println("		Warm up terminé");
+    }
+	
+	private static void resultsToCsv(String pathFile, List<String> requettes,List<HashSet<String>> results) {
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(pathFile,false))) {
+			writer.write("\"Request\",\"Results\"");
+
+			int length = requettes.size();
+			for (int i =0;i<length;i++){
+				writer.newLine();
+				writer.write(requettes.get(i) + "," + results.get(i));
+			}
+		} catch (IOException e) {
+			System.err.println("Erreur lors de l'écriture dans le fichier CSV : " + e.getMessage());
+		}
+	}
+
+	private static void statsToCsv(String pathFile, int nb_requettes, int data_read_time,int query_read_time,int dic_time,int index_time, int eval_time, int tot_time,int nb_no_rep,int nb_rep) {
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(pathFile,true))) {
+			File fichier = new File(pathFile);
+			String qr;
+			if (useFolder) {qr = "folder : " + queryFolder;} else{qr=queryFile;}
+			
+			if (fichier.exists()){
+				writer.write(dataFile +","+ qr +","+ "RDF triplets number" +","+ nb_requettes +","+ data_read_time +","+ query_read_time +","+ dic_time +","+ "1 - OPS" +","+ index_time +","+ eval_time +","+ tot_time +","+ nb_no_rep +","+ nb_rep +","+ "100%");
+				writer.newLine();
+			}
+			else{
+				writer.write("\"Data file name\", \"query file name\", \"RDF triplets number\", \"query number\",\"Data reading time(ms)\",\"Query reading time(ms)\" ,\"Dictionary construction time(ms)\", \"Index number\",\"Index creation time\",\"Workload evaluation time (ms)\",\"Total time(ms)\",\"Nb no response request\",\"Nb response\",\"% response equal to Jena\"");	
+				writer.newLine();
+				writer.write(dataFile +","+ qr +","+ "RDF triplets number" +","+ nb_requettes +","+ data_read_time +","+ query_read_time +","+ dic_time +","+ "1 - OPS" +","+ index_time +","+ eval_time +","+ tot_time +","+ nb_no_rep +","+ nb_rep +","+ "100%");
+			}
+
+		} catch (IOException e) {
+			System.err.println("Erreur lors de la création ou de l'écriture dans le fichier CSV : " + e.getMessage());
+		}
+	}
+
         
 }
